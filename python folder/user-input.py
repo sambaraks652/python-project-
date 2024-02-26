d25c617614acf054bbf14d6d48c917729103dae6
sambaraks652 <sambaraks652@gmail.com>
2024-02-26 09:06:47 +0300

    my user input python code

---
+++ python folder/user-input.py
@@ -0,0 +1,6 @@
+Name = input("What is your name? ")
+Age = int(input("How old are you? " ))
+Location = input("What is your location? ")
+print()
+
+print(f" Hello {Name} you are {Age} years old and your location is {Location}" )
